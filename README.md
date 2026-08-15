6ca1c5d5865e7497c0414a941ff16ce8958a40e1
vdbg <vdb_g@hotmail.com>
2026-08-15 19:56:42 +0200

    V1.0
    Basic import working with proper handling of timezones

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# solark-influx
-Import Sol-Ark data to InfluxDB
+# Solark-influx
+
+Imports data from Sol-Ark to InfluxDB v2.
+
+## Requirements
+
+- Sol-Ark credentials (email/password)
+- InfluxDB v2 instance with write credentials to a bucket
+- Docker or Python 3.11+
+
+## Running the app
+
+The app reads settings from `template.config.toml`, then `config.toml` (if it exists), then environment variables.
+See `template.config.toml` for details.
+
+### From the command line
+
+```bash
+git clone git@github.com:vdbg/solark-influx.git 
+cd solark-influx
+cp template.config.toml config.toml
+# follow instruction to edit
+nano config.toml
+pip3 install -r requirements.txt
+python3 ./main.py
+```
+
+
+### With Docker (no config file)
+
+Inspect `template.config.toml` for the settings to override. 
+
+For example:
+
+```sh
+sudo docker run \
+  -d \
+  --name solark-influx \
+  --pull=always \
+  --restart=always \
+  -e SOLARK_INFLUX_SOLARK_USERNAME=user \
+  -e SOLARK_INFLUX_SOLARK_PASSWORD=password \
+  -e SOLARK_INFLUX_INFLUX_TOKEN=token \
+  -e SOLARK_INFLUX_INFLUX_ORG=my_org \
+  vdbg/solark-influx:latest
+```
+
+Alternatively, you can use a compose.yaml file, optionally with a .env file.
+
+### With Docker (using config file)
+
+1. `sudo docker run --name solark-influx -v config.toml:/app/config.toml vdbg/solark-influx:latest`
+2. `sudo docker cp solark-influx:/app/template.config.toml config.toml`
+3. Edit `config.toml`
+4. `sudo docker start solark-influx -i`
+5. `sudo docker container rm solark-influx`
+6. `sudo docker run -d --name solark-influx -v /path_to_your/config.toml:/app/config.toml --pull=always --restart=always vdbg/solark-influx:latest`
+
---
+++ main.py
@@ -0,0 +1,58 @@
+import logging
+import platform
+import sys
+import time
+from datetime import datetime, timedelta, timezone
+
+from config import Config
+from influx import InfluxConnector
+from solark import SolarkConnector
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+SUPPORTED_PYTHON_MAJOR = 3
+SUPPORTED_PYTHON_MINOR = 11
+
+if sys.version_info < (SUPPORTED_PYTHON_MAJOR, SUPPORTED_PYTHON_MINOR):
+    raise Exception(
+        f"Python version {SUPPORTED_PYTHON_MAJOR}.{SUPPORTED_PYTHON_MINOR} or later required. Current version: {platform.python_version()}."
+    )
+
+try:
+    config = Config("config.toml", "solark_influx").load()
+    main_conf = config["main"]
+    logging.getLogger().setLevel(logging.getLevelName(main_conf["log_verbosity"]))
+    sleep_time_seconds = main_conf["loop_minutes"] * 60
+    logging.debug(f"CONFIG: {config}")
+
+    solarkConnector = SolarkConnector(config["solark"])
+    influxConnector = InfluxConnector(config["influx"])
+
+    while True:
+        try:
+            measurement = influxConnector.measurement
+            to_time_utc = datetime.now(timezone.utc)
+            from_time_utc = influxConnector.get_last_recorded_time_utc(
+                solarkConnector.max_days, to_time_utc, measurement
+            )
+
+            # add 1 day to the current time to ensure we get the last record if it was recorded today
+            while from_time_utc <= to_time_utc + timedelta(days=1):
+                logging.info(f"Querying Sol-Ark for {from_time_utc}...")
+                ret = solarkConnector.get_data(from_time_utc, measurement)
+                if ret:
+                    influxConnector.add_samples(ret)
+                from_time_utc += timedelta(days=1)
+        except Exception as e:
+            logging.exception(e)
+
+        if not sleep_time_seconds:
+            sys.exit(0)
+
+        time.sleep(sleep_time_seconds)
+
+except Exception as e:
+    logging.exception(e)
+    sys.exit(1)
